9bb6822b1846a493944f23dcb441d258d7f5d559
eduardoarcer31 <eduardoarcer31@gmail.com>
2019-03-06 12:29:44 -0500

    Sección 8 terminada

--- src/app/pages/pages.modulo.ts
+++ src/app/pages/pages.modulo.ts
@@ -12,6 +12,9 @@ import { PAGES_ROUTES } from './pages.routes';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+
 
 
 // Temporal
@@ -28,7 +31,10 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
+
     ],
     exports: [
         DashboardComponent,
@@ -40,7 +46,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         PAGES_ROUTES,
         FormsModule,
         ChartsModule
-    ]
+        ]
 
 })
 export class PagesModule {}
---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// tslint:disable-next-line:import-blacklist
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscripcion: Subscription;
+  constructor() {
+
+    this.subscripcion = this.regresaObservable()
+    .subscribe(
+      numero => console.log('subs', numero),
+      error => console.log('Error en el obs', error),
+      () => console.log('El observador terminó')
+    );
+
+   }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('La pagina se va a cerrar');
+    this.subscripcion.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any> {
+
+    return new Observable( (observer: Subscriber<any>) => {
+      let contador = 0;
+      const intervalo = setInterval( () => {
+        contador += 1;
+
+
+        const salida = {
+          valor: contador
+        };
+        observer.next(salida);
+
+        /* if (contador === 3) {
+          clearInterval(intervalo);
+          observer.complete();
+      } */
+        /* if (contador === 2 ) {
+          // clearInterval(intervalo);
+          observer.error('Auxilio!');
+        } */
+      }, 1000);
+
+    }).pipe(
+      map( resp => resp.valor ),
+      filter( (valor, index ) => {
+
+        if ( valor % 2 === 1 ) {
+          return true;
+        } else {
+          return false;
+        }
+
+      })
+    );
+
+  }
+
+}
